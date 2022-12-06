78a3be68f410bbc03f64ad787b135b3984e38ed8
trigubov <trigubovvi@rshb.ru>
2022-12-06 15:04:33 +0300

    Homework v.1.0

--- game_v2.py
+++ game_v2.py
@@ -23,6 +23,30 @@ def random_predict(number: int = 1) -> int:
             break  # выход из цикла если угадали
     return count
 
+def algo_predict(number: int =1,min: int =1,max: int =100) -> int:
+    """Угадываем число по алгоритму: каждая следущая попытка выбирает среднее число между предидущей
+    неудачной попыткой, и максимальным/минимальным возможным вариантом
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+        min (int, optional): Минимально возможное число. Defaults to 1.
+        max (int, optional): Максимально возможное число. Defaults to 100.
+
+    Returns:
+        int: число попыток
+    """
+    count = 0
+    while True:
+        count += 1
+        predict_number = (min + max) // 2
+        if number == predict_number:
+            break  # выход из цикла если угадали
+        elif predict_number > number: # задуманное число - меньше предсказанного
+            max = predict_number - 1 # изменили верхнюю границу поиска
+        elif predict_number < number: # задуманное число - больше предсказанного
+            min = predict_number + 1 # изменили нижнюю границу поиска
+            
+    return count
 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
